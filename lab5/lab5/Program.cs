61d4e0258d97c3958c5e7024b7dcac4f239e1023
cynthia10 <cynthiaagbor37@gmail.com>
2018-05-07 11:54:47 -0400

    lab 4

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = -1;
+            while (input < 0)
+            {
+                input = Question.AskForInterger("enter a positive number");
+            }
+
+            for (int i = 0; i < 10; i++)//continuation asking the ussre the cordinate
+            {
+                System.Console.WriteLine(i);
+            }
+            for (int num=30;num>=20;num--)
+            {
+                System.Console.WriteLine(num);
+            }
+        }
+
+    }
+}
